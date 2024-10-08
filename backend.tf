e581182e9180203e74bb2edf64ea346f22634724
carl5322 <carlos.zavala@rackspace.com>
2024-10-08 16:24:00 -0600

    Delete backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "491085392714-terraforms-state-bucket20241007230952788600000001"
-    key            = "pipeline/us-east-1-terraform_state_file"
-    region         = "us-east-1"
-    dynamodb_table = "infrastructure_management_terraform_state_lock"
-  }
-}
