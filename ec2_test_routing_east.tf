9fa2b7cca04021d1a154eb17a0ed1e3c5af9e61b
carl5322 <carlos.zavala@rackspace.com>
2024-10-17 13:31:37 -0600

    Modify  SG

--- ec2_test_routing_east.tf
+++ ec2_test_routing_east.tf
@@ -84,8 +84,8 @@ resource "aws_security_group" "instance_test_routing" {
   } 
 
    ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 339
+    to_port = 339
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
